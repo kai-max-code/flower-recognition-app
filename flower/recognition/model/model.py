ca6e293420b209f9b79304bc81f04b1d1c49e2b1
kai-max-code <mmcs.8008@gmail.com>
2020-08-27 13:55:52 +0900

    my first commit

---
+++ flower/recognition/model/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from glob import glob
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def initialize_model(num_classes, feature_extract=False, use_pretrained=True):
+    model_ft = models.resnet18(pretrained=use_pretrained)
+    set_parameter_requires_grad(model_ft, feature_extract)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
+    input_size = 224
+    return model_ft
